ab2899be27f7d5891d42615c007cd117cdd5cf74
PetarSimonovic <petar@simonovic.com>
2021-03-09 11:53:16 +0000

    linter changes

--- src/Bank.js
+++ src/Bank.js
@@ -1,34 +1,34 @@
-// Variables
-
-let balance = 0
-let account = []
-let ledger = []
-let statement = ""
-const statement_header = "date || credit || debit || balance\n"
-
-// Functions
-
-function deposit (date, amount) {
-  let newBalance = (balance * 100) + (amount * 100)
-  balance = addDecimal(newBalance)
-  account.unshift([date, amount.toFixed(2), "", balance])
-};
-
-
-function withdrawal (date, amount) {
-  let newBalance = (balance * 100) - (amount * 100)
-  balance = addDecimal(newBalance)
-  account.unshift([date, "", amount.toFixed(2), balance])
-}
-
-function addDecimal (newBalance) {
-  return (newBalance / 100).toFixed(2);
-}
-
-function printStatement () {
-  let index = 0
-  for (index = 0; index < account.length; index++) {
-    ledger.push(account[index].join(' || ').replace('  ', ' '))
+class Bank {
+  constructor () {
+    this.balance = 0
+    this.account = []
+    this.statement = ''
+    this.statementHeader = 'date || credit || debit || balance\n'
+    this.newBalance = 0
   };
-  statement = statement_header.concat(ledger.join('\n'))
+
+  deposit (date, amount) {
+    this.newBalance = (this.balance * 100) + (amount * 100)
+    this.balance = this.addDecimal(this.newBalance)
+    this.account.unshift([date, amount.toFixed(2), '', this.balance])
+  };
+
+  withdrawal (date, amount) {
+    this.newBalance = (this.balance * 100) - (amount * 100)
+    this.balance = this.addDecimal(this.newBalance)
+    this.account.unshift([date, '', amount.toFixed(2), this.balance])
+  }
+
+  addDecimal (newBalance) {
+    return (this.newBalance / 100).toFixed(2)
+  }
+
+  printStatement () {
+    let index = 0
+    let ledger = []
+    for (index = 0; index < this.account.length; index++) {
+      ledger.push(this.account[index].join(' || ').replace('  ', ' '))
+    };
+    this.statement = this.statementHeader.concat(ledger.join('\n'))
+  }
 }
